adb50e728ddd79ac4d68985993558bb82fe2d4d1
Jason Alber <jasonalber17@gmail.com>
2025-07-09 10:30:14 -0400

    Updating the Project string to include the initialized variables and message

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declared and initialized variables
+            
+            string classSort = "Hufflepuff";
+
+            int studentBody = 15000;
+
+            char house = 'H';
+
+            bool ofAge = true;
+            
+            decimal spellsCast = 150.15m;
+            
+            string welcome = "Welcome to Hogwarts!  Please provide us with your name:";
+
+            double gradeAverage = 85.50;
+            
+            //Generated welcome message
+            
+            Console.WriteLine(welcome);
+            
+            string studentName = Console.ReadLine();
+            
+            Console.WriteLine($"Perfect!  Glad to have you with us, {studentName}.  We will get you sorted into your class shortly.");
+            
+            Console.WriteLine($"During you time here, you will be expected to maintain a gradepoint average of {gradeAverage} in each of your classrooms.");
+            
         }
     }
 }
